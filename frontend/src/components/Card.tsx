0231540679290c8f618dfdf064a840e2f49f0021
simongartner-ourliving <simon.gartner@ourliving.se>
2022-04-29 21:43:27 +0200

    Cleaup

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -2,7 +2,7 @@ import { Card as MuiCard, CardProps } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const Card = styled(MuiCard)<CardProps>(() => ({
-  padding: '32px'
+  padding: '32px',
 }));
 
-export default Card
+export default Card;
