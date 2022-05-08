779dd19e32f3ce62e7dd72c2001e136bd8ea8fc7
simongartner-ourliving <simon.gartner@ourliving.se>
2022-05-08 15:49:00 +0200

    fixed error

--- frontend/src/functions/calculateInputResults.ts
+++ frontend/src/functions/calculateInputResults.ts
@@ -3,10 +3,10 @@ import { Company } from './interfaces';
 const excludedAreas = ['operatingProfit', 'profitBeforeTaxes', 'yearResult', 'financialProfit'];
 
 function calculateInputResults(company: Company) {
-  console.log(JSON.parse(JSON.stringify(company.result)));
   return Object.entries(company.result)
-    .map(([section, data]) => {
+    .forEach(([section, data]) => {
       if (excludedAreas.includes(section)) return;
+      // eslint-disable-next-line no-param-reassign
       company.result[section].current = Object.values(data.children)
         .reduce((previous, { current }) => previous + current, 0);
     });
